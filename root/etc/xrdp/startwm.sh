4ca1d2462a8b40ec04324e3a811147402af7f331
Eliran Wong <hkwebtech@gmail.com>
2022-03-05 15:33:07 +0000

    audiotweaked

--- root/etc/xrdp/startwm.sh
+++ root/etc/xrdp/startwm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Disable the following line below on macOS
-PULSE_SCRIPT=/etc/xrdp/pulse/default.pa /startpulse.sh &
+# PULSE_SCRIPT=/etc/xrdp/pulse/default.pa /startpulse.sh &
 # OR:
 # PULSE_SCRIPT=/etc/xrdp/pulse/default.pa /startpulse.sh --start &
 # Start a openbox session
